8ee09a054e34a5d2397b9ea08e636ad8744d5fa0
上术 <10920520+hnitro@user.noreply.gitee.com>
2024-10-27 22:01:57 +0800

    初始化Nunu脚手架

---
+++ cmd/migration/wire/wire.go
@@ -0,0 +1,41 @@
+//go:build wireinject
+// +build wireinject
+
+package wire
+
+import (
+	"go-xianyu/internal/repository"
+	"go-xianyu/internal/server"
+	"go-xianyu/pkg/app"
+	"go-xianyu/pkg/log"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+var repositorySet = wire.NewSet(
+	repository.NewDB,
+	//repository.NewRedis,
+	repository.NewRepository,
+	repository.NewUserRepository,
+)
+var serverSet = wire.NewSet(
+	server.NewMigrate,
+)
+
+// build App
+func newApp(
+	migrate *server.Migrate,
+) *app.App {
+	return app.NewApp(
+		app.WithServer(migrate),
+		app.WithName("demo-migrate"),
+	)
+}
+
+func NewWire(*viper.Viper, *log.Logger) (*app.App, func(), error) {
+	panic(wire.Build(
+		repositorySet,
+		serverSet,
+		newApp,
+	))
+}
